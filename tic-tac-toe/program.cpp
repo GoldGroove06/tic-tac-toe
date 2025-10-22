08d24660a34153e64487bda97736155fd7859647
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-10-22 14:43:01 +0530

    small fixes

--- tic-tac-toe/program.cpp
+++ tic-tac-toe/program.cpp
@@ -32,18 +32,19 @@ class Game{
 				empty--;
 			}
 			else{
-				cout << "enter again";
+				cout << "enter again" << endl;
+				return 1;
 			}
 			return 0;
 		}
 		int checkGame(){
-			for (int i; i<=2; i++){
+			for (int i=0; i<=2; i++){
 				if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' '){
 					empty = 0;
 					cout << board[i][0] << "won the game";
 				}
 			}
-			for (int j; j<=2; j++){
+			for (int j=0; j<=2; j++){
 				if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' '){
                                         empty = 0;
                                         cout << board[j][0] << "won the game";
@@ -79,10 +80,12 @@ int main(){
 		cin >> x;
 		cout << endl << "enter y";
 		cin >> y;
-		g1.insert(input,x,y);
+		int ret = g1.insert(input,x,y);
+		if (ret == 0 ){
 		g1.display();
 		isX = !isX;
 		g1.checkGame();
+		}
 	}
 	g1.display();
 	return 0;
