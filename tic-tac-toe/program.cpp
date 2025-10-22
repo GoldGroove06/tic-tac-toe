a781cbdf1596fccf50a3582fe5b23f536007111f
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-10-22 14:09:48 +0530

    game working

--- tic-tac-toe/program.cpp
+++ tic-tac-toe/program.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 class Game{
 	char board[3][3] = {{' ' , ' ' , ' '},{' ' ,' ', ' '},{' ' , ' ' , ' '}};
-	
 
 	public:
+		int empty = 9;
 		int display(){
 			for (int i = 0; i<=2; i++){
                        		for (int j = 0; j <= 2; j++){
@@ -27,9 +27,39 @@ class Game{
 		}
 
 		int insert(char xo, int locx, int locy){
-			board[locx][locy]=xo;
+			if (board[locx][locy] == ' '){
+				board[locx][locy]=xo;
+				empty--;
+			}
+			else{
+				cout << "enter again";
+			}
 			return 0;
 		}
+		int checkGame(){
+			for (int i; i<=2; i++){
+				if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' '){
+					empty = 0;
+					cout << board[i][0] << "won the game";
+				}
+			}
+			for (int j; j<=2; j++){
+				if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' '){
+                                        empty = 0;
+                                        cout << board[j][0] << "won the game";
+                                }
+			}
+			if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' '){
+                                        empty = 0;
+                                        cout << board[0][0] << "won the game";
+                                }
+			if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' '){
+                                        empty = 0;
+                                        cout << board[0][2] << "won the game";
+                                }
+		}
+
+
 
 
 
@@ -38,7 +68,22 @@ class Game{
 int main(){
 	Game g1;
 	g1.display();
-	g1.insert('X',2,2);
+	bool isX = true;
+	while(g1.empty >0 ){
+		char input;
+		int x;
+		int y;
+		isX ? input = 'X' : input = 'O';
+		isX ? cout << "X turn" : cout << "O turn";
+		cout << endl << "enter x";
+		cin >> x;
+		cout << endl << "enter y";
+		cin >> y;
+		g1.insert(input,x,y);
+		g1.display();
+		isX = !isX;
+		g1.checkGame();
+	}
 	g1.display();
 	return 0;
 }
